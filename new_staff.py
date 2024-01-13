f10e241b379ef19948b7d1e31487d46ef404e852
riley29 <79852273+riley29@users.noreply.github.com>
2024-01-13 14:19:08 +0000

    Update new_staff.py

--- new_staff.py
+++ new_staff.py
@@ -21,12 +21,12 @@ cur.execute(query_insert, (f_name, l_name))     # Run query w/ placeholders defi
 
 query_select = "SELECT staff_id FROM staff_accounts WHERE first_name = %s AND last_name = %s"       # SQL query to fetch primary key (staff_id) for row inserted
 cur.execute(query_select, (f_name, l_name))     # Run query w/ placeholders defined
-staff_id = cur.fetchall()       # Get staff_id 
+staff_id = cur.fetchone()       # Get staff_id 
 print(staff_id)     # Testing purposes only --- REMOVE
 
 query_username = "UPDATE staff_accounts SET user_name = CONCAT(first_name, last_name, floor(random() * 1000 + 1)) WHERE staff_id = %s"      # SQL query to create user_name via concatination of first name, last name & random int
 cur.execute(query_username, (staff_id,))        # Execute query w/ placeholder defined
-query_select_all = "SELECT * FROM staff_accounts WHERE staff_id = %s"       # SQL query to get row of data inserted and updated with user name --- ERROR comparing int to int array
+query_select_all = "SELECT * FROM staff_accounts WHERE staff_id = %s"       # SQL query to get row of data inserted and updated with user name
 cur.execute(query_select_all, (staff_id,))      # Execute query w/ placeholder defined
 new_acc = cur.fetchall()        # Get row
 print(new_acc)      # Print row
