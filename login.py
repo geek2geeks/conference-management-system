6bb87b2bd2edd1fea982efa145466f372b0ac6a3
riley29 <79852273+riley29@users.noreply.github.com>
2024-01-17 14:46:39 +0000

    Update login.py
    Added transaction block error handling

--- login.py
+++ login.py
@@ -18,9 +18,22 @@ class Portal:
         self.cur.close()
         self.conn.close()
 
-    def hash(self, table, column, id, data):
-        query = "INSERT INTO %s (%s) VALUES (crypt(%s, gen_salt('bf'))::bytea) WHERE customer_id = %s;"  # Insert encrypted password(user_key) using bf algorithm as binary(bytea) data type
-        self.cur.execute(query, (table, column, data, id,))
+    def hash(self, table, id, data):
+        if table == 'customer_keys':
+            query = "INSERT INTO customer_keys (customer_id, key) VALUES (%s, crypt(%s, gen_salt('bf'))::bytea);"  # Insert encrypted password(user_key) using bf algorithm as binary(bytea) data type
+        else:
+            query = "INSERT INTO staff_keys (staff_id, key) VALUES (%s, crypt(%s, gen_salt('bf'))::bytea);"
+        self.cur.execute(query, (id, data,))
+
+    def check_curr_block(self):
+        try:
+            self.cur.execute("SELECT * FROM staff_accounts")
+            self.conn.commit()
+        except psycopg2.errors.InFailedSqlTransaction as e:
+            if isinstance(e, psycopg2.DatabaseError) and "aborted" in str(e):
+                self.conn.rollback()
+            else:
+                None
 
 # Method to authenticate staff credentials
     def auth_staff(self, user, key):      # Pass user name & password
@@ -55,14 +68,16 @@ class Portal:
         query = "INSERT INTO staff_accounts (first_name, last_name, user_name) VALUES (%s, %s, %s)"     # SQL query to insert first name, last name into staff_accounts with place holders
         self.cur.execute(query, (f_name, l_name, user_name,))     # Run query w/ placeholders defined
         self.conn.commit()
+        self.close_conn()
 
-    def new_customer(self, f_name, l_name, email, phone_number, company, password):
-        query = "INSERT INTO customer_accounts (first_name, last_name, email, phone_number, company) VALUES (%s, %s, %s, %s, %s)"
-        self.cur.execute(query, (f_name, l_name, email, phone_number, company,))
+    def new_customer(self, f_name, l_name, email, phone_number, company, password):    # Pass first name, last name, email, phone number, company, password
+        self.check_curr_block()     # Check if current block is valid
+        query = "INSERT INTO customer_accounts (first_name, last_name, email, phone_number, company) VALUES (%s, %s, %s, %s, %s)"       # SQL query to insert first name, last name, email, phone number, company into customer_accounts with place holders
+        self.cur.execute(query, (f_name, l_name, email, phone_number, company,))        # Run query w/ placeholders defined
         query_email = ("SELECT customer_id FROM customer_accounts WHERE email = %s")#??? dirty-read
-        self.cur.execute(query_email, (email,))
-        customer_id = self.cur.fetchone() 
-        self.hash('customer_keys', 'key', customer_id, password)
-        self.conn.commit()
-
-close_conn()
+        self.cur.execute(query_email, (email,))     # Execute query
+        customer_id = self.cur.fetchone()       # Define fetched customer id as id
+        table = 'customer_keys'     # Define table for hash method 
+        self.hash(table, customer_id, password)     # Call hash method to hash password
+        self.conn.commit()      # Commit changes to database
+        self.close_conn()       # Close connection
